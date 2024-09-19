daa17200c1d10d96df01649d6603676fcc53c288
Caden Feller <caden.feller@gmail.com>
2024-09-19 16:45:15 -0700

    pong logic completed

--- pongduino/pong.cpp
+++ pongduino/pong.cpp
@@ -17,18 +17,29 @@ private:
         int x;
         int y;
 
+        int score;
+
         // we can change this if needed
         int height = 3;
 
     public:
 
+        Paddle() {
+            x = -1;
+            y = -1;
+        }
+
         Paddle(int x) {
             this -> x = x;
+            y = 3;
+            score = 0;
         }
 
         void up() {y++;}
         void down()  {y--;}
+        void scorePoint() {score++;}
         int getY() {return y;}
+        int getHeight() {return height;}
 
     };
 
@@ -45,6 +56,46 @@ private:
         Ball(int x, int y) {
             this -> x = x;
             this -> y = y;
+        }
+
+        void move() {
+            x += direction[0];
+            y += direction[1];
+        }
+
+        void checkNextColision(Paddle *p1, Paddle *p2) {
+            // if the ball is not near an edge, don't bother calculating anything
+            if (x <= 6 && x >= 2) return;
+
+            // decide which paddle to detect based on ball's position
+            Paddle player = x > 6 ? *p2 : *p1;
+
+            // the x and y positions the ball will be in on the next tick
+            int xStep = x + direction[0];
+            int yStep = y + direction[1];
+
+            if (yStep >= player.getY() && yStep <= player.getY()+player.getHeight()) {
+                direction[0] *= -1; 
+                direction[1] = x - (player.getY() + (player.getHeight() / 2));
+                // cap the vertical speed
+                if (direction[1] > 3) {direction[1] = 3;}
+            }
+
+            if (yStep < 0 || yStep > 7) {
+                direction[0] *= -1;
+                // account for any overflow that would happen on the next tick
+                y -= direction[0];
+            }
+
+            if (xStep < 0 || xStep > 7) {
+                direction[0] *= -1;
+                direction[1] = 1;
+                x = 3;
+                y = 3;
+                player.scorePoint();
+            }
+
+
         }
 
         void setDirection(int x, int y) {
@@ -74,13 +125,15 @@ private:
 
     };
 
+    Paddle player1;
+    Paddle player2;
+    Ball ball;
 
 public:
     
-    Pong() {
+    Pong(): ball(3, 3), player1(0), player2(7) {
         renderBoard(BoardTypes::INIT);
     }
-
 
     // this method is really just doing different things to the entire board 
     // so we only have to write one double-for loop
@@ -102,8 +155,13 @@ public:
             }
         }
     }
+    // funny pyramid function ^
 
-
-
+    void tick() {
+        ball.move(); 
+        // TODO: accept user input and use the up() and down() methods on each paddle to control them 
+        ball.checkNextColision(&player1, &player2);
+        renderBoard(BoardTypes::OUTPUT);
+    }
     
 };
